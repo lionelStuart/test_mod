01aa5660c1f01d4e3d636249d0613c196872225d
lionelStuart <lisi_stuart@126.com>
2019-07-26 23:57:51 +0800

    init go mod

---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+import "github.com/lionelStuart/test_mod/test
+
+func main() {
+	PrintHelp()
+	fmt.Println("123")
+}
---
+++ test/help.go
@@ -0,0 +1,7 @@
+package test
+
+import "fmt"
+
+func PrintHelp() {
+	fmt.Println("print help")
+}
